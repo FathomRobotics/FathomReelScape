d9f55aced821ba091f38e27c2f48b42e42546728
turtle4831 <jakethrun@gmail.com>
2025-02-18 18:05:42 -0600

    new bot initial commit

--- src/main/java/frc/robot/LastKnownPositions.java
+++ src/main/java/frc/robot/LastKnownPositions.java
@@ -1,7 +1,6 @@
 package frc.robot;
 
 public class LastKnownPositions {
-    public static double ElevatorPivotLastKnownPose = 0;
     public static double ElevatorLastKnownPose = 0;
     public static double WristLastKnownPose = 0; //might be in 3.60
     
